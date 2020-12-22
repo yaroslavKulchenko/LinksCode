85dc99544c92c53c5887336de870d2e449a89f2c
yaroslavKulchenko <perekatipole123123@gmail.com>
2020-12-22 17:41:20 +0300

    Pochti the end

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -41,7 +41,7 @@ export const AuthPage = () => {
   return (
     <div className="row">
       <div className="col s6 offset-s3">
-        <h1>Сократи Ссылку</h1>
+        <h1>Курсовая работа</h1>
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <span className="card-title">Авторизация</span>
